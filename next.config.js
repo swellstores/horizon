a60b381e91143d58d1d8964bb49d2b7039fb509b
Alex Tuti <107462252+alextutii@users.noreply.github.com>
2022-12-13 17:16:02 +0200

    feat: fetch store locales in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isDev = process.env.NODE_ENV === 'development';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+let nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.schema.io', ...(isDev ? ['cdn.swell.test'] : [])],
@@ -16,11 +17,6 @@ const nextConfig = {
 
     return config;
   },
-  // TODO: Get from GraphQL
-  i18n: {
-    locales: ['en', 'es-ES'],
-    defaultLocale: 'en',
-  },
   rewrites() {
     return [
       {
@@ -31,4 +27,27 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = async () => {
+  const storeUrl = process.env.NEXT_PUBLIC_SWELL_STORE_URL;
+  const graphqlKey = process.env.NEXT_PUBLIC_SWELL_PUBLIC_KEY;
+  const getLocales = async () => {
+    const headers = {
+      Authorization: graphqlKey,
+    };
+    const res = await fetch(`${storeUrl}/api/settings`, {
+      headers,
+    });
+    const data = await res.json();
+
+    return {
+      locales: data.store.locales.map((locale) => locale.code),
+      defaultLocale: data.store.locales.map((locale) => locale.code)[0],
+    };
+  };
+
+  const i18n = await getLocales();
+
+  nextConfig.i18n = i18n;
+
+  return nextConfig;
+};
