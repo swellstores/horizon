56433b080128a7569bef9271acb7f2ea35caf164
Cristian Stănescu <cristian@swell.is>
2023-05-30 20:25:29 +0300

    Rework env variables

--- build-utils/sync-storefront.mjs
+++ build-utils/sync-storefront.mjs
@@ -4,65 +4,65 @@ import fs from 'fs';
 
 dotenv.config();
 
-const STORE_ID = process.env.SWELL_STORE_ID;
-const STOREFRONT_ID = process.env.SWELL_STOREFRONT_ID;
-const SECRET_KEY = process.env.SWELL_SECRET_KEY;
+const requiredEnvVars = [
+  'SWELL_STORE_ID',
+  'SWELL_STOREFRONT_ID',
+  'SWELL_SECRET_KEY',
+];
 
-if (!STORE_ID) {
-  throw new Error('Missing SWELL_STORE_ID');
-}
-
-if (!STOREFRONT_ID) {
-  throw new Error('Missing SWELL_STOREFRONT_ID');
-}
-
-if (!SECRET_KEY) {
-  throw new Error('Missing SWELL_SECRET_KEY');
-}
+requiredEnvVars.forEach((envVar) => {
+  if (!process.env[envVar]) {
+    throw new Error(`Missing ${envVar}`);
+  }
+});
 
 function executeCommand(command) {
-  exec(command.join(' '), (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
+  exec(
+    command.join(' '),
+    { env: { ...process.env } },
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
 
-    console.log(stdout || stderr);
-  });
+      console.log(stdout || stderr);
+    },
+  );
 }
 
 executeCommand([
   'swell storefronts toggle-editor',
-  `--id ${STOREFRONT_ID}`,
+  `--id $SWELL_STOREFRONT_ID`,
   '--theme-id horizon',
   '--enable true',
-  `--secret-key ${SECRET_KEY}`,
-  `--store ${STORE_ID}`,
+  `--secret-key $SWELL_SECRET_KEY`,
+  `--store $SWELL_STORE_ID`,
 ]);
 
 executeCommand([
   'swell storefronts push',
-  `--id ${STOREFRONT_ID}`,
+  '--id $SWELL_STOREFRONT_ID',
   '-t editor',
   '-f ./config/editor.json',
-  `--secret-key ${SECRET_KEY}`,
-  `--store ${STORE_ID}`,
+  '--secret-key $SWELL_SECRET_KEY',
+  '--store $SWELL_STORE_ID',
 ]);
 
 executeCommand([
   'swell storefronts push',
-  `--id ${STOREFRONT_ID}`,
+  '--id $SWELL_STOREFRONT_ID',
   '-t settings',
   '-f ./config/defaults.json',
-  `--secret-key ${SECRET_KEY}`,
-  `--store ${STORE_ID}`,
+  '--secret-key $SWELL_SECRET_KEY',
+  '--store $SWELL_STORE_ID',
 ]);
 
 fs.readdirSync('./config/content').forEach((file) => {
   executeCommand([
     'swell model push -c',
     `-f ./config/content/${file}`,
-    `--secret-key ${SECRET_KEY}`,
-    `--store ${STORE_ID}`,
+    '--secret-key $SWELL_SECRET_KEY',
+    '--store $SWELL_STORE_ID',
   ]);
 });
